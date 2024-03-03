12c93424ac53c7a4c0ad86532837a49f22d950ea
Railton-Carvalho <railtonplayrx@gmail.com>
2024-03-03 09:15:21 -0300

    Project Created

---
+++ src/main/java/com/rocket/rain/apigateaway/GeoSystemApplication.java
@@ -0,0 +1,13 @@
+package com.rocket.rain.apigateaway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeoSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeoSystemApplication.class, args);
+	}
+
+}
