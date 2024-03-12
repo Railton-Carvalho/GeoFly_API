abb4d6e5160fb19749ac29f3e6196385d5849d0b
Railton-Carvalho <railtonplayrx@gmail.com>
2024-03-12 10:53:17 -0300

    feat: MethodArgumentNotValidException on ResourceExceptionHandler class

--- src/main/java/com/rocket/rain/apigateaway/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/rocket/rain/apigateaway/resources/exceptions/ResourceExceptionHandler.java
@@ -5,11 +5,16 @@ import com.rocket.rain.apigateaway.services.exceptions.ResourceNotFoundException
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @RestControllerAdvice//intercepta as excecao para que o obj possa executar algum tipo de tratamento
 public class ResourceExceptionHandler{
@@ -26,7 +31,6 @@ public class ResourceExceptionHandler{
         String error = "DataBase error";
         HttpStatus status = HttpStatus.BAD_REQUEST;
         StandardError err = new StandardError(Instant.now(),status.value(),error,e.getMessage(),request.getRequestURI());
-        System.out.println(request.getMethod());
         return ResponseEntity.status(status).body(err);
     }
     @ExceptionHandler(NoResourceFoundException.class)
@@ -34,8 +38,28 @@ public class ResourceExceptionHandler{
         String error = "Controller not Found";
         HttpStatus status = HttpStatus.NOT_FOUND;
         StandardError err = new StandardError(java.time.Instant.now(),status.value(),error,e.getMessage(), request.getRequestURI());
-        System.out.println(request.getMethod());
         return ResponseEntity.status(status).body(err);
     }
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<StandardError> methodarugumentNotValid(MethodArgumentNotValidException e,
+                                                                 HttpServletRequest request){
+        String error = "Arguments Missed";
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        System.out.println(e.getFieldErrors().toString());
+        StandardError err = new StandardError(java.time.Instant.now(),status.value(),error,
+               extractFieldsWithError(e.getFieldErrors().toString()),
+                request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+    public static String extractFieldsWithError(String errorMessage) {
+        //reponsael por filtrar a mensagem de erro retornada pela exception ArgumentNotValid
+        List<String> fieldNames = new ArrayList<>();
+        Pattern pattern = Pattern.compile("on field '(.*?)'");
+        Matcher matcher = pattern.matcher(errorMessage);
 
+        while (matcher.find()) {
+            fieldNames.add(matcher.group(1));
+        }
+        return String.join(", ", fieldNames);
+    }
 }
